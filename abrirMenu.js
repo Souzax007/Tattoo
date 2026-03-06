e16162ce695135cc764ef2d69bae2ce84db1055f
Marcos <marcos@albesbrasil.com>
2026-03-06 11:09:31 -0300

    inicio da responsividade.

--- abrirMenu.js
+++ abrirMenu.js
@@ -3,17 +3,58 @@ const menu = document.querySelector(".btn-acao");
 const overlay = document.querySelector(".overlay");
 
 toggle.addEventListener("click", () => {
-
     toggle.classList.toggle("ativo");
     menu.classList.toggle("ativo");
     overlay.classList.toggle("ativo");
-
 });
 
 overlay.addEventListener("click", () => {
-
     toggle.classList.remove("ativo");
     menu.classList.remove("ativo");
     overlay.classList.remove("ativo");
-
 });
+
+
+
+const imagens = document.querySelectorAll(".ilustracao .i");
+const container = document.querySelector(".ilustracao");
+
+let index = 0;
+let intervalo;
+
+
+function trocarImagem(){
+
+    imagens.forEach((img)=>{
+        img.classList.remove("ativo");
+    });
+
+    imagens[index].classList.add("ativo");
+
+    index++;
+
+    if(index >= imagens.length){
+        index = 0;
+    }
+
+}
+
+
+function iniciarCarrossel(){
+    intervalo = setInterval(trocarImagem,1000);
+}
+
+
+function pararCarrossel(){
+    clearInterval(intervalo);
+}
+
+
+trocarImagem();
+iniciarCarrossel();
+
+
+container.addEventListener("mouseenter", pararCarrossel);
+
+
+container.addEventListener("mouseleave", iniciarCarrossel);
